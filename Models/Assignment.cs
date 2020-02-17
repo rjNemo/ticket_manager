e57b7359c763f866a550e92a1d717ba96f70d9d5
Ruidy Nemausat <ruidy.nemausat@gmail.com>
2020-02-17 15:35:43 +0100

    projRepo integrated

--- Models/Assignment.cs
+++ Models/Assignment.cs
@@ -4,7 +4,6 @@ namespace TicketManager.Models
 {
     public class Assignment
     {
-        // public int Id { get; set; }
         public AppUser User { get; set; }
         public Guid UserId { get; set; }
         public Project Project { get; set; }
