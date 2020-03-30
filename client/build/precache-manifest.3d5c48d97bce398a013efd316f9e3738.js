b7a6d56f4135d86f1d66fc8e66e49250f02bbff4
Ruidy Nemausat <ruidy.nemausat@gmail.com>
2020-03-30 16:21:22 +0200

    client build

---
+++ client/build/precache-manifest.3d5c48d97bce398a013efd316f9e3738.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cebf0ab6a622ed59e1f466d61ce6fba8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4a9969b0ef2953f804f2",
+    "url": "/static/js/2.f2d11b5c.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/static/js/2.f2d11b5c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4f5cfbb7c1db784041ca",
+    "url": "/static/js/main.db31bacc.chunk.js"
+  },
+  {
+    "revision": "5d93c6b2d15332364552",
+    "url": "/static/js/runtime-main.cea588d5.js"
+  }
+]);
