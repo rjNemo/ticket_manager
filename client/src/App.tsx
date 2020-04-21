a99c4450491327ef8a24bcdd3764110863562507
Ruidy Nemausat <ruidy.nemausat@gmail.com>
2020-04-21 07:11:51 +0200

    Update Preloader component

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -4,15 +4,14 @@ import { useAuth0 } from "./authentication/auth0";
 import MainLayout from "./layouts/MainLayout";
 import AppRouter from "./routes/AppRouter";
 import history from "./utils/history";
+import Preloader from "./components/Preloader";
 
 export default function App() {
   const { loading } = useAuth0();
 
-  if (loading) {
-    return <div>Loading...</div>;
-  }
-
-  return (
+  return loading ? (
+    <Preloader />
+  ) : (
     <Router history={history}>
       <MainLayout>
         <AppRouter />
